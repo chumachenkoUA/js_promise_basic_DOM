47179fc290a96d2c59e9b6d1b7113bea7380a47c
chumachenkoUA <kirito22122005@gmail.com>
2025-11-05 21:49:05 +0200

    implement promises with error handling in main.js; update demo link in README.md

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+function promises() {
+  const promiseOne = new Promise((resolve) => {
+    logo.addEventListener('click', () => resolve('Promise was resolved!'));
+  });
+
+  const promiseTwo = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+
+  promiseOne
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+      body.append(div);
+    })
+    .catch((err) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message error-message');
+      div.textContent = err.message;
+      body.append(div);
+    });
+
+  promiseTwo
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+      body.append(div);
+    })
+    .catch((err) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message', 'error-message');
+      div.textContent = err.message;
+      body.append(div);
+    });
+}
+
+promises();
