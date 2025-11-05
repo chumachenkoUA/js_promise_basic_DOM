f73f50fd2a2c1bff16f9f56e999dacaf292b9583
chumachenkoUA <kirito22122005@gmail.com>
2025-11-05 21:49:50 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,38 @@
+"use strict";
+const body = document.querySelector("body");
+const logo = document.querySelector(".logo");
+function promises() {
+    const promiseOne = new Promise((resolve)=>{
+        logo.addEventListener("click", ()=>resolve("Promise was resolved!"));
+    });
+    const promiseTwo = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            reject(new Error("Promise was rejected!"));
+        }, 3000);
+    });
+    promiseOne.then((message)=>{
+        const div = document.createElement("div");
+        div.classList.add("message");
+        div.textContent = message;
+        body.append(div);
+    }).catch((err)=>{
+        const div = document.createElement("div");
+        div.classList.add("message error-message");
+        div.textContent = err.message;
+        body.append(div);
+    });
+    promiseTwo.then((message)=>{
+        const div = document.createElement("div");
+        div.classList.add("message");
+        div.textContent = message;
+        body.append(div);
+    }).catch((err)=>{
+        const div = document.createElement("div");
+        div.classList.add("message", "error-message");
+        div.textContent = err.message;
+        body.append(div);
+    });
+}
+promises();
+
+//# sourceMappingURL=index.f75de5e1.js.map
